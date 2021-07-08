6e2b20e8740fe1f476b0393fd4255667f96d5b02
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-07-08 12:19:53 -0700

    hints

--- cmake/Finducc.cmake
+++ cmake/Finducc.cmake
@@ -1,5 +1,5 @@
-find_library(ucc_LIBRARY NAMES ucc)
-find_path(ucc_INCLUDE_DIR NAMES api/ucc.h)
+find_library(ucc_LIBRARY NAMES ucc HINTS "/opt/ucc/lib" "/opt/ucf/ucc/lib")
+find_path(ucc_INCLUDE_DIR NAMES api/ucc.h HINTS "/opt/ucc/include" "/opt/ucf/ucc/include")
 
 set(ucc_LIBRARIES "${ucc_LIBRARY}")
 set(ucc_INCLUDE_DIRS "${ucc_INCLUDE_DIR}")
