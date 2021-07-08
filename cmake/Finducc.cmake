37406b8fa2f09b1f331e9cb0c308b137b33a87da
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-07-08 12:12:09 -0700

    save

--- cmake/Finducc.cmake
+++ cmake/Finducc.cmake
@@ -1,3 +1,10 @@
-find_library(FOO_LIBRARY NAMES ucc)
-find_path(FOO_INCLUDE_DIR NAMES api/ucc.h)
-message("Hello! ucc")
+find_library(ucc_LIBRARY NAMES ucc)
+find_path(ucc_INCLUDE_DIR NAMES api/ucc.h)
+
+set(ucc_LIBRARIES "${ucc_LIBRARY}")
+set(ucc_INCLUDE_DIRS "${ucc_INCLUDE_DIR}")
+
+add_library(ucc::ucc INTERFACE IMPORTED)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ucc DEFAULT_MSG ucc_LIBRARY ucc_INCLUDE_DIR)
