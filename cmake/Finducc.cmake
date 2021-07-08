666d0ae445c87021cf1f20dcf3800c1459dcdfff
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-07-08 13:09:32 -0700

    save

--- cmake/Finducc.cmake
+++ cmake/Finducc.cmake
@@ -1,10 +1,48 @@
-find_library(ucc_LIBRARY NAMES ucc HINTS "/opt/ucc/lib" "/opt/ucf/ucc/lib")
-find_path(ucc_INCLUDE_DIR NAMES api/ucc.h HINTS "/opt/ucc/include" "/opt/ucf/ucc/include")
+# Reference:
+# https://cmake.org/cmake/help/latest/manual/cmake-developer.7.html#find-modules
 
-set(ucc_LIBRARIES "${ucc_LIBRARY}")
-set(ucc_INCLUDE_DIRS "${ucc_INCLUDE_DIR}")
+find_package(PkgConfig)
+pkg_check_modules(PC_ucc QUIET ucc)
 
-add_library(ucc::ucc INTERFACE IMPORTED)
+find_path(ucc_INCLUDE_DIR
+  NAMES ucc/api/ucc.h
+  PATHS ${PC_ucc_INCLUDE_DIRS}
+  HINTS "/opt/ucc/include" "/opt/ucf/ucc/include"
+)
+find_library(ucc_LIBRARY
+  NAMES ucc
+  PATHS ${PC_ucc_LIBRARY_DIRS}
+  HINTS "/opt/ucc/lib" "/opt/ucf/ucc/lib"
+)
+
+# TODO: read version from ucc/api/ucc_version.h
+set(ucc_VERSION ${PC_ucc_VERSION})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ucc DEFAULT_MSG ucc_LIBRARY ucc_INCLUDE_DIR)
+find_package_handle_standard_args(ucc
+  FOUND_VAR ucc_FOUND
+  REQUIRED_VARS
+    ucc_LIBRARY
+    ucc_INCLUDE_DIR
+  VERSION_VAR ucc_VERSION
+)
+
+if(ucc_FOUND)
+  set(ucc_LIBRARIES ${ucc_LIBRARY})
+  set(ucc_INCLUDE_DIRS ${ucc_INCLUDE_DIR})
+  set(ucc_DEFINITIONS ${PC_ucc_CFLAGS_OTHER})
+endif()
+
+if(ucc_FOUND AND NOT TARGET ucc::ucc)
+  add_library(ucc::ucc UNKNOWN IMPORTED)
+  set_target_properties(ucc::ucc PROPERTIES
+    IMPORTED_LOCATION "${ucc_LIBRARY}"
+    INTERFACE_COMPILE_OPTIONS "${PC_ucc_CFLAGS_OTHER}"
+    INTERFACE_INCLUDE_DIRECTORIES "${ucc_INCLUDE_DIR}"
+  )
+endif()
+
+mark_as_advanced(
+  Foo_INCLUDE_DIR
+  Foo_LIBRARY
+)
