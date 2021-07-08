2138f4723b6a837b170fad62d6aaf5848bbf319f
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-07-08 16:40:20 -0700

    save

--- run_ucx.sh
+++ run_ucx.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
 set -eux
+
 build/ucx/ucp_client_server &
 sleep 1
 build/ucx/ucp_client_server -a 127.0.0.1
+wait
+
+build/ucx/ucp_hello_world &
+sleep 1
+build/ucx/ucp_hello_world -n 127.0.0.1
+wait
+
+ip addr
+build/ucx/uct_hello_world -d eth0 -t tcp &
+sleep 1
+build/ucx/uct_hello_world -d eth0 -t tcp  -n 127.0.0.1
 wait
