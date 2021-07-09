b305c64ac29c60c90b06a62c62e15897d09903ce
Xiang <qasdfgtyuiop@gmail.com>
2021-07-09 16:35:30 -0700

    save

--- test/ucx/test_uct.c
+++ test/ucx/test_uct.c
@@ -4,13 +4,13 @@
 int main(int argc, char **argv)
 {
     ucs_async_context_t *async;
-    iface_info_t if_info;
+    uct_worker_h worker;
 
     /* Initialize context */
     status = ucs_async_context_create(UCS_ASYNC_MODE_THREAD_SPINLOCK, &async);
     assert(UCS_OK == status);
 
     /* Create a worker object */
-    status = uct_worker_create(async, UCS_THREAD_MODE_SINGLE, &if_info.worker);
+    status = uct_worker_create(async, UCS_THREAD_MODE_SINGLE, &worker);
     assert(UCS_OK == status);
 }
