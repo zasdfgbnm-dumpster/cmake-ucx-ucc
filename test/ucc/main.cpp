63ed5cf38b2ce021a37e71eda545d90f9b4986b6
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-07-09 16:16:54 -0700

    Update main.cpp

--- test/ucc/main.cpp
+++ test/ucc/main.cpp
@@ -9,10 +9,11 @@ int main(int argc, char *argv[]) {
   ucc_lib_h lib;
 
   st = ucc_lib_config_read(nullptr, nullptr, &lib_config);
-  assert(st == UCC_OK, "Failed to read config");
+  assert(st == UCC_OK);
   memset(&lib_params, 0, sizeof(ucc_lib_params_t));
   lib_params.mask = UCC_LIB_PARAM_FIELD_THREAD_MODE;
   lib_params.thread_mode = UCC_THREAD_MULTIPLE;
   st = ucc_init(&lib_params, lib_config, &lib);
+  assert(st == UCC_OK);
   ucc_lib_config_release(lib_config);
 }
