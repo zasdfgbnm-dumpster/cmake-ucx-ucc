24610f6e87a092a133fff9829aa1f814f06573ef
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-07-08 17:12:25 -0700

    enable

--- run_ucx.sh
+++ run_ucx.sh
@@ -7,12 +7,15 @@ set -eux
 # build/ucx/ucp_client_server -a 127.0.0.1
 # wait
 
-# build/ucx/ucp_hello_world &
-# sleep 1
-# build/ucx/ucp_hello_world -n 127.0.0.1
-# wait
+build/ucx/ucp_hello_world &
+pid0=$!
+sleep 1
+build/ucx/ucp_hello_world -n 127.0.0.1
+pid0=$1
+wait "${pid0}"
+wait "${pid1}"
 
-# ip addr
+ip addr
 # build/ucx/uct_hello_world -d eth0 -t tcp &
 # sleep 1
 # build/ucx/uct_hello_world -d eth0 -t tcp  -n 127.0.0.1
