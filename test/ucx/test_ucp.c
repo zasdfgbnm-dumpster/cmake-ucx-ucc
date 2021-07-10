c653acd0f2ae408e6a3e724ddc270af6186a670b
Xiang <qasdfgtyuiop@gmail.com>
2021-07-09 17:18:57 -0700

    save

--- test/ucx/test_ucp.c
+++ test/ucx/test_ucp.c
@@ -15,14 +15,4 @@ int main(int argc, char **argv)
 
     status = ucp_init(&ucp_params, NULL, ucp_context);
     assert(status == UCS_OK);
-
-    ucp_worker_params_t worker_params;
-
-    memset(&worker_params, 0, sizeof(worker_params));
-
-    worker_params.field_mask  = UCP_WORKER_PARAM_FIELD_THREAD_MODE;
-    worker_params.thread_mode = UCS_THREAD_MODE_SINGLE;
-
-    status = ucp_worker_create(ucp_context, &worker_params, ucp_worker);
-    assert(status == UCS_OK);
 }
