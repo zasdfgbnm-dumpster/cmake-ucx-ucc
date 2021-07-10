39989276d4391c65df2cd4c4db610c11da499afe
Xiang <qasdfgtyuiop@gmail.com>
2021-07-09 17:29:36 -0700

    save

--- test/ucx/test_ucp.c
+++ test/ucx/test_ucp.c
@@ -4,14 +4,11 @@
 int main(int argc, char **argv)
 {
     ucp_context_h ucp_context;
-    ucp_worker_h ucp_worker;
     ucp_params_t ucp_params;
     ucs_status_t status;
-
     memset(&ucp_params, 0, sizeof(ucp_params));
-
-    // ucp_params.field_mask = UCP_PARAM_FIELD_FEATURES;
-    // ucp_params.features = UCP_FEATURE_STREAM;
+    ucp_params.field_mask = UCP_PARAM_FIELD_FEATURES;
+    ucp_params.features = UCP_FEATURE_STREAM;
 
     status = ucp_init(&ucp_params, NULL, ucp_context);
     assert(status == UCS_OK);
