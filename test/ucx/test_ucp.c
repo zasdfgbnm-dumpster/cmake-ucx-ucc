bcf99891692d58729716225ad148f3f4d7385876
Xiang <qasdfgtyuiop@gmail.com>
2021-07-09 17:01:17 -0700

    save

--- test/ucx/test_ucp.c
+++ test/ucx/test_ucp.c
@@ -5,6 +5,6 @@ int main(int argc, char **argv)
 {
     ucp_context_h ucp_context;
     ucp_worker_h ucp_worker;
-    ret = init_context(&ucp_context, &ucp_worker, CLIENT_SERVER_SEND_RECV_DEFAULT);
+    auto ret = init_context(&ucp_context, &ucp_worker, CLIENT_SERVER_SEND_RECV_DEFAULT);
     assert(ret == 0);
 }
